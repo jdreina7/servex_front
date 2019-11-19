c779a0a641d1dc339f9943ca4bdaed88dc2a1c22
Juan David Reina <judareiro@gmail.com>
2019-11-18 23:17:42 -0500

    Ajustes en paginas y ruta de api en heroku

--- src/app/pages/content-pages/content-pages-routing.module.ts
+++ src/app/pages/content-pages/content-pages-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ComingSoonPageComponent } from './coming-soon/coming-soon-page.component';
 import { ErrorPageComponent } from './error/error-page.component';
-import { ForgotPasswordPageComponent } from './forgot-password/forgot-password-page.component';
-import { LockScreenPageComponent } from './lock-screen/lock-screen-page.component';
 import { LoginPageComponent } from './login/login-page.component';
-import { MaintenancePageComponent } from './maintenance/maintenance-page.component';
-import { RegisterPageComponent } from './register/register-page.component';
 import { ServexgalleryComponent } from './servexgallery/servexgallery/servexgallery.component';
 
 
@@ -16,31 +11,18 @@ const routes: Routes = [
     path: '',
     children: [
       {
-        path: 'comingsoon',
-        component: ComingSoonPageComponent,
-        data: {
-          title: 'Coming Soon page'
-        }
-      },
-      {
         path: 'error',
         component: ErrorPageComponent,
         data: {
           title: 'Error Page'
         }
       },
+
       {
-        path: 'forgotpassword',
-        component: ForgotPasswordPageComponent,
+        path: 'servexgallery',
+        component: ServexgalleryComponent,
         data: {
-          title: 'Forgot Password Page'
-        }
-      },
-      {
-        path: 'lockscreen',
-        component: LockScreenPageComponent,
-        data: {
-          title: 'Lock Screen page'
+          title: 'Servex Gallery'
         }
       },
       {
@@ -50,27 +32,7 @@ const routes: Routes = [
           title: 'Login Page'
         }
       },
-      {
-        path: 'maintenance',
-        component: MaintenancePageComponent,
-        data: {
-          title: 'Maintenance Page'
-        }
-      },
-      // {
-      //   path: 'register',
-      //   component: RegisterPageComponent,
-      //   data: {
-      //     title: 'Register Page'
-      //   }
-      // }
-      {
-        path: 'servexgallery',
-        component: ServexgalleryComponent,
-        data: {
-          title: 'Servex Gallery'
-        }
-      }
+
     ]
   }
 ];
